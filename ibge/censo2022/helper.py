f7b6e65aa6a49024f0bf0d9fa90ec4a35789d79d
Fabricio_nascimento <fabricio.nascimentop2024@gmail.com>
2026-09-09 14:21:28 -0300

    finalizado (eu acho)

---
+++ ibge/censo2022/helper.py
@@ -0,0 +1,189 @@
+regioes = {
+    # Região Araguaia
+    "150034": "Araguaia",  # Água Azul do Norte
+    "150125": "Araguaia",  # Bannach
+    "150270": "Araguaia",  # Conceição do Araguaia
+    "150276": "Araguaia",  # Cumaru do Norte
+    "150304": "Araguaia",  # Floresta do Araguaia
+    "150543": "Araguaia",  # Ourilândia do Norte
+    "150555": "Araguaia",  # Pau D'Arco
+    "150613": "Araguaia",  # Redenção
+    "150616": "Araguaia",  # Rio Maria
+    "150658": "Araguaia",  # Santa Maria das Barreiras
+    "150670": "Araguaia",  # Santana do Araguaia
+    "150730": "Araguaia",  # São Félix do Xingu
+    "150775": "Araguaia",  # Sapucaia
+    "150808": "Araguaia",  # Tucumã
+    "150840": "Araguaia",  # Xinguara
+
+    # Região Baixo Amazonas
+    "150040": "Baixo Amazonas",  # Alenquer
+    "150050": "Baixo Amazonas",  # Almeirim
+    "150145": "Baixo Amazonas",  # Belterra
+    "150285": "Baixo Amazonas",  # Curuá
+    "150300": "Baixo Amazonas",  # Faro
+    "150390": "Baixo Amazonas",  # Juruti
+    "150475": "Baixo Amazonas",  # Mojuí dos Campos
+    "150480": "Baixo Amazonas",  # Monte Alegre
+    "150510": "Baixo Amazonas",  # Óbidos
+    "150530": "Baixo Amazonas",  # Oriximiná
+    "150600": "Baixo Amazonas",  # Prainha
+    "150680": "Baixo Amazonas",  # Santarém
+    "150797": "Baixo Amazonas",  # Terra Santa
+
+    # Região Guamá
+    "150240": "Guamá",  # Castanhal
+    "150260": "Guamá",  # Colares
+    "150290": "Guamá",  # Curuçá
+    "150320": "Guamá",  # Igarapé-Açu
+    "150340": "Guamá",  # Inhangapi
+    "150410": "Guamá",  # Magalhães Barata
+    "150430": "Guamá",  # Maracanã
+    "150440": "Guamá",  # Marapanim
+    "150700": "Guamá",  # Santo Antônio do Tauá
+    "150660": "Guamá",  # Santa Maria do Pará
+    "150650": "Guamá",  # Santa Izabel do Pará
+    "150710": "Guamá",  # São Caetano de Odivelas
+    "150720": "Guamá",  # São Domingos do Capim
+    "150740": "Guamá",  # São Francisco do Pará
+    "150746": "Guamá",  # São João da Ponta
+    "150760": "Guamá",  # São Miguel do Guamá
+    "150796": "Guamá",  # Terra Alta
+    "150820": "Guamá",  # Vigia
+
+    # Região Carajás
+    "150157": "Carajás",  # Bom Jesus do Tocantins
+    "150175": "Carajás",  # Brejo Grande do Araguaia
+    "150215": "Carajás",  # Canaã dos Carajás
+    "150277": "Carajás",  # Curionópolis
+    "150295": "Carajás",  # Eldorado dos Carajás
+    "150420": "Carajás",  # Marabá
+    "150549": "Carajás",  # Palestina do Pará
+    "150553": "Carajás",  # Parauapebas
+    "150563": "Carajás",  # Piçarra
+    "150715": "Carajás",  # São Domingos do Araguaia
+    "150745": "Carajás",  # São Geraldo do Araguaia
+    "150750": "Carajás",  # São João do Araguaia
+
+    # Região Lago Tucuruí
+    "150178": "Lago Tucuruí",  # Breu Branco
+    "150309": "Lago Tucuruí",  # Goianésia do Pará
+    "150370": "Lago Tucuruí",  # Itupiranga
+    "150380": "Lago Tucuruí",  # Jacundá
+    "150497": "Lago Tucuruí",  # Nova Ipixuna
+    "150506": "Lago Tucuruí",  # Novo Repartimento
+    "150810": "Lago Tucuruí",  # Tucuruí
+
+    # Região Marajó
+    "150070": "Marajó",  # Anajás
+    "150110": "Marajó",  # Bagre
+    "150180": "Marajó",  # Breves
+    "150200": "Marajó",  # Cachoeira do Arari
+    "150250": "Marajó",  # Chaves
+    "150280": "Marajó",  # Curralinho
+    "150310": "Marajó",  # Gurupá
+    "150450": "Marajó",  # Melgaço
+    "150490": "Marajó",  # Muaná
+    "150570": "Marajó",  # Ponta de Pedras
+    "150580": "Marajó",  # Portel
+    "150630": "Marajó",  # Salvaterra
+    "150640": "Marajó",  # Santa Cruz do Arari
+    "150770": "Marajó",  # São Sebastião da Boa Vista
+    "150790": "Marajó",  # Soure
+    "150030": "Marajó",  # Afuá
+
+    # Região Rio Caeté
+    "150090": "Rio Caeté",  # Augusto Corrêa
+    "150160": "Rio Caeté",  # Bonito
+    "150170": "Rio Caeté",  # Bragança
+    "150195": "Rio Caeté",  # Cachoeira do Piriá
+    "150220": "Rio Caeté",  # Capanema
+    "150500": "Rio Caeté",  # Nova Timboteua
+    "150560": "Rio Caeté",  # Peixe-Boi
+    "150610": "Rio Caeté",  # Primavera
+    "150611": "Rio Caeté",  # Quatipuru
+    "150620": "Rio Caeté",  # Salinópolis
+    "150655": "Rio Caeté",  # Santa Luzia do Pará
+    "150690": "Rio Caeté",  # Santarém Novo
+    "150747": "Rio Caeté",  # São João de Pirabas
+    "150803": "Rio Caeté",  # Tracuateua
+    "150830": "Rio Caeté",  # Viseu
+
+    # Região Tapajós
+    "150100": "Tapajós",  # Aveiro
+    "150360": "Tapajós",  # Itaituba
+    "150375": "Tapajós",  # Jacareacanga
+    "150503": "Tapajós",  # Novo Progresso
+    "150619": "Tapajós",  # Rurópolis
+    "150805": "Tapajós",  # Trairão
+
+    # Região Tocantins
+    "150010": "Tocantins",  # Abaetetuba
+    "150020": "Tocantins",  # Acará
+    "150120": "Tocantins",  # Baião
+    "150130": "Tocantins",  # Barcarena
+    "150210": "Tocantins",  # Cametá
+    "150330": "Tocantins",  # Igarapé-Miri
+    "150400": "Tocantins",  # Limoeiro do Ajuru
+    "150460": "Tocantins",  # Mocajuba
+    "150470": "Tocantins",  # Moju
+    "150520": "Tocantins",  # Oeiras do Pará
+    "150795": "Tocantins",  # Tailândia
+
+    # Região Guajará
+    "150080": "Guajará",  # Ananindeua
+    "150140": "Guajará",  # Belém
+    "150150": "Guajará",  # Benevides
+    "150442": "Guajará",  # Marituba
+    "150635": "Guajará",  # Santa Bárbara do Pará
+
+    # Região Rio Capim
+    "150013": "Rio Capim",  # Abel Figueiredo
+    "150095": "Rio Capim",  # Aurora do Pará
+    "150190": "Rio Capim",  # Bujaru
+    "150230": "Rio Capim",  # Capitão Poço
+    "150275": "Rio Capim",  # Concórdia do Pará
+    "150293": "Rio Capim",  # Dom Eliseu
+    "150307": "Rio Capim",  # Garrafão do Norte
+    "150345": "Rio Capim",  # Ipixuna do Pará
+    "150350": "Rio Capim",  # Irituia
+    "150405": "Rio Capim",  # Mãe do Rio
+    "150495": "Rio Capim",  # Nova Esperança do Piriá
+    "150540": "Rio Capim",  # Ourém
+    "150550": "Rio Capim",  # Paragominas
+    "150618": "Rio Capim",  # Rondon do Pará
+    "150800": "Rio Capim",  # Tomé-Açu
+    "150812": "Rio Capim",  # Ulianópolis
+
+    # Região Xingu
+    "150060": "Xingu",  # Altamira
+    "150085": "Xingu",  # Anapu
+    "150172": "Xingu",  # Brasil Novo
+    "150548": "Xingu",  # Pacajá
+    "150565": "Xingu",  # Placas
+    "150590": "Xingu",  # Porto de Moz
+    "150780": "Xingu",  # Senador José Porfírio
+    "150815": "Xingu",  # Uruará
+    "150835": "Xingu",  # Vitória do Xingu
+    "150445": "Xingu",  # MEDICILÂNDIA 
+}
+
+def data_to_ano(df):
+    import pandas as pd
+    df['Data'] = pd.to_datetime(df['Data'], format="%m/%Y")
+    df["Ano"] = df["Data"].dt.year
+    df["Mes"] = df["Data"].dt.month
+    
+    return df
+
+def codigo_to_ri(df):
+    df = df.copy()
+
+    df["Codigo"] = (
+        df["Codigo"]
+        .astype("string")
+        .str.replace(r"\.0$", "", regex=True)
+        .str.zfill(6)
+    )
+    df["RI"] = df["Codigo"].map(regioes)
+    return df
